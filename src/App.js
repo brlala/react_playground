c7455c195e89f279cce2f4fb876ec613513a529f
liheng <liheng@pand.ai>
2020-12-16 18:36:40 +0800

    changing states from children component(input)

--- src/App.js
+++ src/App.js
@@ -8,11 +8,30 @@ function App() {
     {name: 'Manu', age: 30},
     {name: 'Jessie', age: 41},
   ]);
+
+  const switchNameHandler = (newName) => {
+    console.log('was clicked')
+    setPersons([
+      {name: newName, age: 2},
+      {name: 'ManU', age: 3},
+      {name: 'JessiE', age: 4},
+    ])
+  }
+
+  const nameChangeHandler = (event) => {
+    console.log('was clicked')
+    setPersons([
+      {name: event.target.value, age: 2},
+      {name: "Manu", age: 3},
+      {name: 'JessiE', age: 4},
+    ])
+  }
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
+      <button onClick={() => switchNameHandler('xxxxxxx')}>Switch Name</button>
       <Person name={persons[0].name}/>
-      <Person name={persons[1].age}>My gender: Girl</Person>
+      <Person name={persons[1].name} click={switchNameHandler} change={nameChangeHandler}>My gender: Girl</Person>
       <Person name='Ronnie'/>
     </div>
   );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 const person = (props) => {
   return (
     <div>
-      <p>I'm a person called {props.name}!</p>
-      <p>{props.children}</p>
+      <p onClick={props.click}>I'm a person called {props.name}!</p>
+      <p >{props.children}</p>
+      <input type="text" onChange={props.change}/>
     </div>
   )
 }
