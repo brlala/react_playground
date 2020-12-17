50ad079e5c69b11f0bf1f0fac9a0de3859a3a94f
liheng <liheng@pand.ai>
2020-12-17 23:15:36 +0800

    added linter to package

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
 import './App.css';
-import Person from './Person/Person'
-import React, {useState} from 'react';
+import { useState } from 'react';
+import Person from './Person/Person';
 
 function App() {
   const [persons, setPersons] = useState([
-    {name: 'Max', age: 28},
-    {name: 'Manu', age: 30},
-    {name: 'Jessie', age: 41},
+    { name: 'Max', age: 28 },
+    { name: 'Manu', age: 30 },
+    { name: 'Jessie', age: 41 },
   ]);
+  const [showPerson, setShowPerson] = useState(false);
   const style = {
     backgroundColor: 'white',
     font: 'inherit',
     border: '1px solid blue',
     padding: '8px',
     cursor: 'pointer',
-  }
+  };
 
   const switchNameHandler = (newName) => {
-    console.log('was clicked')
+    console.log('was clicked');
     setPersons([
-      {name: newName, age: 2},
-      {name: 'ManU', age: 3},
-      {name: 'JessiE', age: 4},
-    ])
-  }
+      { name: newName, age: 2 },
+      { name: 'ManU', age: 3 },
+      { name: 'JessiE', age: 4 },
+    ]);
+  };
 
   const nameChangeHandler = (event) => {
-    console.log('was clicked')
+    console.log('was clicked');
     setPersons([
-      {name: 'Max', age: 2},
-      {name: event.target.value, age: 3},
-      {name: 'JessiE', age: 4},
-    ])
-  }
+      { name: 'Max', age: 2 },
+      { name: event.target.value, age: 3 },
+      { name: 'JessiE', age: 4 },
+    ]);
+  };
+
+  const togglePersonsHandler = () => {
+    setShowPerson(!showPerson);
+  };
+
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <button
+        type="button"
         style={style}
-        onClick={() => switchNameHandler('xxxxxxx')}>Switch Name</button>
-      <Person name={persons[0].name} click={() => switchNameHandler('test')} />
-      <Person name={persons[1].name} change={nameChangeHandler}>My gender: Girl</Person>
-      <Person name='Ronnie'/>
+        onClick={togglePersonsHandler}
+      >
+        Switch Name
+      </button>
+      <div>
+        <Person name={persons[0].name} click={() => switchNameHandler('test')} />
+        <Person name={persons[1].name} change={nameChangeHandler}>My gender: Girl</Person>
+        <Person name="Ronnie" />
+      </div>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,15 @@
-import './Person.css'
-const person = (props) => {
-  return (
-    <div className="Person">
-      <p onClick={props.click}>I'm a person called {props.name}!</p>
-      <p >{props.children}</p>
-      <input type="text" onChange={props.change} value={props.name}/>
-    </div>
-  )
-}
+import './Person.css';
+
+const person = (props) => (
+  <div className="Person">
+    <p onClick={props.click}>
+      I'm a person called
+      {props.name}
+      !
+    </p>
+    <p>{props.children}</p>
+    <input type="text" onChange={props.change} value={props.name} />
+  </div>
+);
 
 export default person;
