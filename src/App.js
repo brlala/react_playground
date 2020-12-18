cd766b82fa1c3e36e170a63d661ccda8563312f7
liheng <liheng@pand.ai>
2020-12-18 11:33:33 +0800

    css modules

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import './App.css';
+// import './App.module.css';
 import { useState } from 'react';
-import styled from 'styled-components';
+import styles from './App.module.css';
 import Person from './Person/Person';
 
-const StyledButton = styled.button`
-  background-color: ${(props) => (props.alt ? 'red' : 'green')};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${(props) => (props.alt ? 'salmon' : 'lightgreen')};
-    color: black;
-  }
-`;
-
-function App(border = '1px solid blue') {
+function App() {
   const [persons, setPersons] = useState([
     { id: 'af1', name: 'Max', age: 28 },
     { id: 'af2', name: 'Manu', age: 30 },
     { id: 'af3', name: 'Jessie', age: 41 },
   ]);
   const [showPerson, setShowPerson] = useState(false);
-  const style = {
-    backgroundColor: 'green',
-    color: 'white',
-    font: 'inherit',
-    border,
-    padding: '8px',
-    cursor: 'pointer',
-    ':hover': {
-      backgroundColor: 'lightgreen',
-      color: 'black',
-    },
-  };
 
   const switchNameHandler = (newName) => {
     console.log('was clicked');
@@ -68,6 +42,7 @@ function App(border = '1px solid blue') {
   };
 
   let personsPanel;
+  let btnClass = '';
   if (showPerson) {
     personsPanel = (
       <div>
@@ -79,44 +54,39 @@ function App(border = '1px solid blue') {
             change={(event) => nameChangeHandler(event, person.id)}
           />
         ))}
-        {/* <Person name={persons[1].name} click={() => switchNameHandler('test')} /> */}
-        {/* <Person name={persons[2].name} change={nameChangeHandler}>My gender: Girl</Person> */}
       </div>
     );
-    // style.backgroundColor = 'red';
-    // style[':hover'] = {
-    //   backgroundColor: 'salmon',
-    //   color: 'black',
-    // };
+
+    btnClass = styles.Red;
   }
-  const classes = [];
+  const assignedClasses = [];
   if (persons.length <= 2) {
-    classes.push('red');
+    assignedClasses.push(styles.red);
   }
   if (persons.length <= 1) {
-    classes.push('bold');
+    assignedClasses.push(styles.bold);
   }
   return (
-    <div className="App">
+    <div className={styles.App}>
       <h1>Hi, I'm a React App</h1>
-      <p className={classes.join(' ')}>This is working</p>
-      <StyledButton
+      <p className={assignedClasses.join(' ')}>This is working</p>
+      <button
         key="asd"
         type="button"
         // style={style}
-        alt={showPerson}
         onClick={togglePersonsHandler}
+        className={btnClass}
       >
         Toggle Name
-      </StyledButton>
-      <StyledButton
+      </button>
+      <button
         type="button"
         // style={style}
-        alt={showPerson}
         onClick={() => switchNameHandler('xxxxxxx')}
+        className={btnClass}
       >
         Switch Name
-      </StyledButton>
+      </button>
       {personsPanel}
     </div>
   );
